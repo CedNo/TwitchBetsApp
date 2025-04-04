17b53806b1542903cfa26a365fd07b816076b1a4
Cedric <cedric.noel.2@ulaval.ca>
2025-04-04 14:27:32 -0400

    Fixed mock bets ids

--- app/constants.ts
+++ app/constants.ts
@@ -2,15 +2,15 @@ import type { Bet } from './types/bet';
 
 export const BETS: Bet[] = [
     {id:'1', title: 'Will @Someone stream today? aaaaaaaaa aaaaaaaaaa aaaaaaa (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.45}, {option: 'No', odds: 0.35} , {option: 'Maybe', odds: 0.20}],  image: '/coin.png'},
-    {id:'1', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
-    {id:'1', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
-    {id:'1', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
-    {id:'1', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
-    {id:'1', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
-    {id:'1', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
-    {id:'1', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
-    {id:'1', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
-    {id:'1', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
-    {id:'1', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
-    {id:'1', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'}
+    {id:'2', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
+    {id:'3', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
+    {id:'4', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
+    {id:'5', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
+    {id:'6', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
+    {id:'7', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
+    {id:'8', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
+    {id:'9', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
+    {id:'10', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
+    {id:'11', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'},
+    {id:'12', title: 'Will @Someone stream today? (02/04/2025)', betOptions: [{option: 'Yes', odds: 0.65}, {option: 'No', odds: 0.35}],  image: '/coin.png'}
 ];
