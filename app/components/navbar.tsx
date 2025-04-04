4aa95048a366e17267f77b46c2aa2cf81575a881
Cedric <cedric.noel.2@ulaval.ca>
2025-04-04 15:28:45 -0400

    Fixed Button component cursor type

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -7,10 +7,10 @@ export default function Navbar() {
     return (
         <div className="items-center justify-between flex p-4 bg-secondary-bg">
             <Link href={"/"}><div className="w-fit shrink-0 cursor-pointer flex items-center font-bold"><h1 className='text-2xl'>TwitchBets</h1></div></Link> {/* Logo */}
-            <div className="w-fit shrink-0 flex items-center justify-center gap-2">
-                <Button className="hover:bg-sky-700 rounded px-2 py-1 cursor-pointer">Login</Button>
-                <Button className="bg-sky-700 px-2 py-1 rounded hover:bg-sky-800 cursor-pointer">Sign Up</Button>
-                <Button className="hover:bg-sky-700 rounded px-2 py-1 cursor-pointer"><IoMenu size={25}/></Button>
+            <div className="w-fit shrink-0 flex items-center justify-center gap-2 [&>*]:py-1 [&>*]:px-2 [&>*]:rounded">
+                <Button className="hover:bg-sky-700">Login</Button>
+                <Button className="bg-sky-700 hover:bg-sky-800">Sign Up</Button>
+                <Button className="hover:bg-sky-700"><IoMenu size={25}/></Button>
             </div>
         </div>
     );
