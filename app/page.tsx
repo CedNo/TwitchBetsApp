3737318d55820d09bbc11b459466e11ac05b4a33
Cedric <cedric.noel.2@ulaval.ca>
2025-04-04 14:16:17 -0400

    Added navbar and footer to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,15 @@
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
 import BetCard from "./components/bet-card";
 import {BETS} from "./constants";
 
-const betCards = BETS.map((bet, index) => (
-    <BetCard key={index} bet={bet} />
-));
-
 export default function Home() {
+  
+  const betCards = BETS.map((bet, index) => (
+    <BetCard key={index} bet={bet} />
+  ));
+
   return (
-    <div className="flex flex-col h-screen justify-between">
-      <Navbar/>
-      <div className='my-10 m-auto w-5/6 items-center justify-center flex columns-4 flex-wrap'>
-        {betCards}
-      </div>
-      <Footer/>
+    <div className='my-10 m-auto w-5/6 items-center justify-center flex columns-4 flex-wrap'>
+      {betCards}
     </div>
   );
 }
