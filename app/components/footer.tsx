3efb555dbef1f27576292bac1413bc4cb9e5e338
Cedric <cedric.noel.2@ulaval.ca>
2025-04-05 14:39:03 -0400

    Fixed footer responsiveness

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -14,10 +14,9 @@ export default function Footer() {
                     <p className="text-sm">All rights reserved</p>
                 </div>
             </div>
-            <div className="flex items-center justify-between gap-2 columns-3 divide-neutral-300 divide-x-1 *:h-full">
-                <div className="flex flex-col gap-4 w-full p-4">
-                    <div className="flex flex-row gap-4 justify-between px-10 py-5">
-                        <ul className="flex flex-col items-start justify-center gap-4 whitespace-nowrap text-l font-bold">
+            <div className="flex flex-col sm:flex-row items-center justify-between gap-2 divide-foreground divide-y-1 sm:divide-y-0 sm:divide-x-1 *:h-full">
+                <div className="w-full flex flex-row gap-10 p-10 justify-around">
+                        <ul className=" flex flex-col items-start justify-center gap-4 whitespace-nowrap text-l font-bold">
                             <li>Your profile</li>
                             <li>Browse</li>
                             <li>Stats</li>
@@ -29,46 +28,43 @@ export default function Footer() {
                             <li>More pages</li>
                             <li>More pages</li>
                         </ul>
-                        <ul className="flex flex-col items-start justify-center gap-4 whitespace-nowrap text-l font-bold">
+                        <ul className="hidden lg:flex flex-col items-start justify-center gap-4 whitespace-nowrap text-l font-bold">
                             <li>More pages</li>
                             <li>More pages</li>
                             <li>More pages</li>
                             <li>More pages</li>
                         </ul>
-                        <ul className="flex flex-col items-start justify-center gap-4 whitespace-nowrap text-l font-bold">
+                        <ul className="hidden lg:flex flex-col items-start justify-center gap-4 whitespace-nowrap text-l font-bold">
                             <li>More pages</li>
                             <li>More pages</li>
                             <li>More pages</li>
                             <li>More pages</li>
                         </ul>
-                    </div>
                 </div>
-                <div className="flex flex-row gap-10 p-10">
+                <div className="flex flex-row gap-10 p-10 w-full sm:w-fit justify-around">
                     <ul className="flex flex-col gap-4 whitespace-nowrap text-l font-bold my-auto">
                         <li>About</li>
                         <li>Contact</li>
                         <li>Privacy Policy</li>
                         <li>Terms of Service</li>
                     </ul>
-                    <ul className="flex flex-col gap-4 whitespace-nowrap text-l font-bold my-auto">
+                    <ul className="flex sm:hidden lg:flex flex-col gap-4 whitespace-nowrap text-l font-bold my-auto">
                         <li>More links</li>
                         <li>More links</li>
                         <li>More links</li>
                         <li>More links</li>
                     </ul>
                 </div>
-                <div className="flex flex-col p-10">
-                    <div className="flex flex-col items-center gap-6 whitespace-nowrap text-l **:size-7 my-auto">
-                        <Link href={'https://github.com/CedNo/TwitchBetsApp'}>
-                            <BsTwitterX />
-                        </Link>
-                        <Link href={'https://github.com/CedNo/TwitchBetsApp'}>
-                            <FaDiscord />
-                        </Link>
-                        <Link href={'https://github.com/CedNo/TwitchBetsApp'}>
-                            <FaGithub />
-                        </Link>
-                    </div>
+                <div className="flex flex-row sm:flex-col items-center gap-6 whitespace-nowrap text-l **:size-7 my-auto p-10">
+                    <Link href={'https://github.com/CedNo/TwitchBetsApp'}>
+                        <BsTwitterX />
+                    </Link>
+                    <Link href={'https://github.com/CedNo/TwitchBetsApp'}>
+                        <FaDiscord />
+                    </Link>
+                    <Link href={'https://github.com/CedNo/TwitchBetsApp'}>
+                        <FaGithub />
+                    </Link>
                 </div>
             </div>
         </footer>
