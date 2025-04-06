52f100f40abedca168acafbd9602ab27a90a24a8
Cedric <cedric.noel.2@ulaval.ca>
2025-04-06 18:01:03 -0400

    Added show/hide password feature

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,21 +1,53 @@
+'use client'
+
 import Button from "@/app/components/button";
 import Link from "next/link";
+import { useState } from 'react'
+import { FaEye } from "react-icons/fa6";
+import { FaEyeSlash } from "react-icons/fa6";
 
 export default function Login() {
+
+    const eye = <FaEye className="absolute mr-10" size={25}/>;
+    const eyeOff = <FaEyeSlash className="absolute mr-10" size={25}/>;
+
+    const [password, setPassword] = useState("");
+    const [type, setType] = useState('password');
+    const [icon, setIcon] = useState(eyeOff);
+
+    const handleToggle = () => {
+        if (type === 'password'){
+           setIcon(eye);
+           setType('text')
+        } else {
+           setIcon(eyeOff)
+           setType('password')
+        }
+     }
+
     return (
         <div className="px-4 md:px-0">
             <form className="w-full md:w-1/2 lg:w-5/16 flex flex-col gap-4 my-10 mx-auto bg-secondary-bg rounded-xl p-10 shadow-xl">
                 <h1 className="text-3xl font-bold text-center pb-4">Welcome back!</h1>
                 <input
+                    className="p-2 border rounded-md w-full focus:outline-none focus:border-secondary-button-hover"
                     type="text"
                     placeholder="Username"
-                    className="p-2 border rounded-md"
                 />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    className="p-2 border rounded-md"
-                />
+                <div className="flex items-center">
+                    <input
+                        className="w-full pr-8 border p-2 rounded-md focus:outline-none focus:border-secondary-button-hover"
+                        type={type}
+                        name="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        autoComplete="current-password"
+                    />
+                    <span className="flex justify-around h-[25px]" onClick={handleToggle}>
+                        {icon}
+                    </span>
+                </div>
                 <div className="flex flex-row justify-between text-sm">
                     <label className="flex items-center gap-2">
                         <input type="checkbox" className="w-3 h-3" />
