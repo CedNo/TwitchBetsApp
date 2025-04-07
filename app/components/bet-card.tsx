60efe532a97a810665ba7a075a27740634dcdebf
Cédric Noël <70913418+CedNo@users.noreply.github.com>
2025-04-07 18:21:15 -0400

    Fixed centering on BetCard title (#7)

--- app/components/bet-card.tsx
+++ app/components/bet-card.tsx
@@ -6,7 +6,7 @@ export default function BetCard({ bet }: { bet: Bet }) {
     return (
         <Link href={`/bet/${encodeURIComponent(bet.id)}`}>
             <div className="h-40 w-80 bg-secondary-bg rounded-lg flex flex-col items-center p-4 m-2 shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out cursor-pointer">
-                <div className="flex items-center justify-start mb-2">
+                <div className="flex items-center justify-start mb-2 mr-auto">
                     <Image src={bet.image} width={100} height={100} alt="Bet Image" className="w-8 h-8 rounded-full mr-2" />
                     <h3 className="text-sm font-bold line-clamp-2">{bet.title}</h3>
                 </div>
