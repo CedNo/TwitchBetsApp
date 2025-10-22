9f6eccd9e86f1a82510a248eb8c776741bccf0e5
Cedric <cedric.noel.2@ulaval.ca>
2025-10-22 19:07:16 -0400

    Sort BetOptions by their odds on BetCard

--- app/components/bet-card.tsx
+++ app/components/bet-card.tsx
@@ -13,10 +13,10 @@ export default function BetCard({ betQuestion }: { betQuestion : BetQuestion }) 
                 </div>
                 <div className="pb-2 flex flex-col overflow-y-scroll items-start w-full">
                     {
-                        Array.from(betQuestion.options).map((option, index) => (
+                        Array.from(betQuestion.options).sort((a, b) => b.odds - a.odds).map((option, index) => (
                             <div key={index} className="flex flex-row items-center justify-between border-solid border-foreground border-1 w-full p-1 rounded-md mb-1 text-sm pl-2 pr-2">
                                 <div>{option.option}</div>
-                                <div>{option.odds}%</div>
+                                <div>{(option.odds * 100).toFixed(2)}%</div>
                             </div>
                         ))
                     }
