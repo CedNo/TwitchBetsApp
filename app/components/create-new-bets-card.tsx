ad8c0fc3386c22ec0958acdd566c0b47467d58fc
Cedric <cedric.noel.2@ulaval.ca>
2026-01-28 15:51:41 -0500

    Used BetCardContainer for CreateNewBetsCard

--- app/components/create-new-bets-card.tsx
+++ app/components/create-new-bets-card.tsx
@@ -1,12 +1,13 @@
 import { CiCirclePlus } from "react-icons/ci";
+import BetCardContainer from "./bet-card-container";
 
 export default function CreateNewBetCard() {
     return (
-        <div className="h-40 w-80 bg-secondary-bg rounded-lg flex flex-col items-center p-4 m-2">
+        <BetCardContainer>
             <div className="flex space-x-2 my-auto">
                 <CiCirclePlus className="text-4xl"/>
                 <p className="font-bold my-auto">Create a new bet question</p>
             </div>
-        </div>
+        </BetCardContainer>
     );
 }
