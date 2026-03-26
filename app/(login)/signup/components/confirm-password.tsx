f11c1cc1279829f497f439790cc08c12565a657d
Cedric <cedric.noel.2@ulaval.ca>
2026-03-25 21:49:13 -0400

    Added password validation to password fields

--- app/(login)/signup/components/confirm-password.tsx
+++ app/(login)/signup/components/confirm-password.tsx
@@ -7,30 +7,53 @@ export default function ConfirmPasswordField() {
 
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
-
-    let conflict = false;
-
-    if(password === confirmPassword || password === "" || confirmPassword === "") {
-        conflict = false;
-    } else {
-        conflict = true;
-    }
+    const [isValidMatch, setIsValidMatch] = useState(true);
 
     return(
-        <div className="flex flex-col gap-4">
-            <PasswordField
-                className={conflict ? "border-red-500 focus:!border-red-400" : ""}
-                value={password}
-                onChange={
-                    (e) => setPassword(e)
-                }/>
-            <PasswordField
-                className={conflict ? "border-red-500 focus:!border-red-400" : ""}
-                placeholder="Confirm Password"
-                value={confirmPassword}
-                onChange={
-                    (e) => setConfirmPassword(e)
-                }/>
+        <div className="flex flex-col gap-1">
+            <div className="flex flex-col gap-4">
+                <PasswordField
+                    className={(!isPasswordLengthValid(password) && password !== "") || (!isValidMatch && confirmPassword !== "") ? "border-red-500 focus:!border-red-400" : ""}
+                    value={password}
+                    onChange={
+                        (e) => {
+                            setPassword(e);
+                            setIsValidMatch(passwordsMatch(e, confirmPassword));
+                        }
+                    }
+                />
+                <PasswordField
+                    className={!isValidMatch && confirmPassword !== "" ? "border-red-500 focus:!border-red-400" : ""}
+                    placeholder="Confirm Password"
+                    value={confirmPassword}
+                    onChange={
+                        (e) => {
+                            setConfirmPassword(e);
+                            setIsValidMatch(passwordsMatch(password, e));
+                        }
+                    }
+                />
+            </div>
+            {(!isPasswordLengthValid(password) && password !== "") ?
+                <p className="text-red-500 text-sm">
+                    Password must be between 8 and 64 characters.
+                </p> : 
+                <p className="text-sm invisible">|</p>
+            }
+            {(!isValidMatch && confirmPassword !== "") ?
+                <p className="text-red-500 text-sm">
+                    Passwords do not match.
+                </p> : 
+                <p className="text-sm invisible">|</p>
+            }
         </div>
     );
+}
+
+function isPasswordLengthValid(password: string) {
+    return password.length >= 8 && password.length <= 64;
+}
+
+function passwordsMatch(password: string, confirmPassword: string) {
+    return password === confirmPassword;
 }
